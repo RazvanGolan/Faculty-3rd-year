fe187776176059bdb29f6133702df832551db6a6
RazvanGolan <razvanbitea@gmail.com>
2025-03-10 17:26:16 +0200

    lftc made file path dynamic for better portability

--- lftc/AtomC/files/tests/testlex.c
+++ lftc/AtomC/files/tests/testlex.c
@@ -1,18 +1,38 @@
-// program de testare a analizorului lexical, v1.1
-
-// identificator -> variable names, int, else double, etc
-// eroare daca nu ai numar dupa virgula la double
-// la &&, || verifica daca apar amadoi, else error
-
 #include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
 #include "../utils.h"
 #include "../lexer.h"
 
+char *getAbsolutePath(const char *relativePath)
+{
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        chdir("..");
+
+        if (getcwd(cwd, sizeof(cwd)) != NULL)
+        {
+            char *absPath = malloc(PATH_MAX);
+            snprintf(absPath, PATH_MAX, "%s/%s", cwd, relativePath);
+            return absPath;
+        }
+    }
+    perror("getcwd() error");
+    return NULL;
+}
+
 int main()
 {
-    const char *fileName = "/Users/razvybitea/Faculty-3rd-year/lftc/AtomC/files/tests/test.c";
-    char *buf = loadFile(fileName);
+    const char *relativePath = "AtomC/files/tests/test.c";
+    char *fileName = getAbsolutePath(relativePath);
+    if (fileName == NULL)
+    {
+        return 1;
+    }
 
+    char *buf = loadFile(fileName);
     const Token *tokens = tokenize(buf);
 
     showTokens(tokens);
